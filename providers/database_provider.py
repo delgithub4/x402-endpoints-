fc1410323eff1ceca9c3be9d233db0ed8d690287
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 22:34:27 +0100

    Create database_provider.py

---
+++ providers/database_provider.py
@@ -0,0 +1,13 @@
+class DatabaseProvider:
+
+    def session(self):
+
+        return None
+
+    async def connect(self):
+
+        return True
+
+    async def disconnect(self):
+
+        return True
